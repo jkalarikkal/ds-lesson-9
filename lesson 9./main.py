a47dc4a46b44d030a36ba3ef427eca739f54fe09
jkalarikkal <jkalarikkal007@gmail.com>
2026-03-04 18:58:43 +0300

    Update main.py

--- lesson 9./main.py
+++ lesson 9./main.py
@@ -9,6 +9,44 @@ table.info()
 tick = table.groupby("Ticker")["Adj_Close"].mean()
 print(tick)
 
-mat.bar(tick.index, tick)
-mat.title('S and P adjisted prices')
+#mat.bar(tick.index, tick)
+#mat.title('S and P adjisted prices')
+#mat.show()
+
+
+#to find mean of all the days for each company (high + low mean)
+
+meanie = table.groupby("Ticker")[["High","Low"]].mean()
+
+
+meanie["mean"] = (meanie["High"] + meanie["Low"])/2
+print(meanie)
+
+'''
+mat.bar(meanie.index, meanie["mean"])
+mat.title("Means of the companies")
+mat.show()
+'''
+
+#best performing day for each company 
+
+lampie = table.groupby("Ticker")["Adj_Close"].idxmax()
+print(lampie)
+
+# bob = table.loc[lampie]
+# print(bob)
+
+
+# mat.scatter(bob["Ticker"], bob["Adj_Close"], color = "red", label = "ended close")
+# mat.scatter(bob["Ticker"], bob["High"], color = "blue", label = "highest" )
+# mat.legend()
+# mat.title("Comaprisons of company volatility")
+# mat.show()
+
+
+#lowest 'low' of every company 
+
+lowey = table.groupby("Ticker")["Low"].min()
+mat.bar(lowey.index, lowey)
+mat.title("Lowest of the low", color = "red")
 mat.show()
